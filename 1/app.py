e79db8a5612f137797518e45d388120945e8fab9
olegstepit <129774659+olegstepit@users.noreply.github.com>
2023-11-22 20:39:39 +0200

    Create app.py

---
+++ 1/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_system_info():
+    data = {
+        'host': request.host,
+        'ip': request.remote_addr,
+        'user_agent': request.headers.get('User-Agent'),
+        'method': request.method,
+        'scheme': request.scheme,
+    }
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
